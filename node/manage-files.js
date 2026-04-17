a5d084e3bde2e262f61c99a4e2dc1a5d3d87c5e5
Manu del Aguila <tockra@gmail.com>
2026-04-17 15:34:27 +0200

    Refactor file management permissions and update package dependencies to remove caret notation

--- node/manage-files.js
+++ node/manage-files.js
@@ -1,18 +1,27 @@
 import { mkdir, readFile, writeFile } from 'node:fs/promises'
-import { join, basename, extname } from "node:path"
+import { join, basename, extname } from 'node:path'
 
-const content = await readFile("archivo.txt", "utf-8")
+let content = ''
 
-console.log(content)
+if (process.permission?.has('fs.read', 'archivo.txt')) {
+    content = await readFile('archivo.txt', 'utf-8')
+    console.log(content)
+} else {
+    console.log('No tienes permiso para leer el archivo.')
+}
 
-const outputDir = join("output", "files", "documents")
-await mkdir(outputDir, {recursive: true})
+if (process.permission?.has('fs.write', 'output/files/documents')) {
+    const outputDir = join('output', 'files', 'documents')
+    await mkdir(outputDir, { recursive: true })
 
-const uppercaseContent = content.toUpperCase()
-const outputFilePath = join(outputDir, "archivo-uppercase.txt")
+    const uppercaseContent = content.toUpperCase()
+    const outputFilePath = join(outputDir, 'archivo-uppercase.txt')
 
-console.log('La extensión es: ', extname(outputFilePath))
-console.log('El nombre del archivo es: ', basename(outputFilePath))
+    console.log('La extensión es: ', extname(outputFilePath))
+    console.log('El nombre del archivo es: ', basename(outputFilePath))
 
-await writeFile(outputFilePath, uppercaseContent)
-console.log("Archivo en mayusculas creado")
+    await writeFile(outputFilePath, uppercaseContent)
+    console.log('Archivo creado con contenido en mayúsculas')
+} else {
+    console.log('No tienes permiso para escribir en el directorio especificado.')
+}
